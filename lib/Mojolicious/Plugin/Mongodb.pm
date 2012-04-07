0e70bea3453a5fbed04dc6c9c5ef346e4e07d247
Ben van Staveren <madcat@cpan.org>
2012-04-08 04:03:42 +0700

    findAndModify and mapReduce helpers as well as some architectural changes

--- lib/Mojolicious/Plugin/Mongodb.pm
+++ lib/Mojolicious/Plugin/Mongodb.pm
@@ -11,29 +11,89 @@ sub register {
 
     $conf->{helper} ||= 'db';
 
-    $app->attr('defaultdb' => sub { delete($conf->{'database'}) || undef });
-    $app->attr('mongodb_connection' => sub { MongoDB::Connection->new($conf) });
-    $app->helper('connection' => sub {
-        my $c = shift;
-        warn q|Mojolicious::Plugin::Mongodb: the 'connection' attribute is deprecated, please use 'mongodb_connection' instead!|, "\n";
-        return $c->mongodb_connection;
+    $app->attr('_mongodb' => sub { 
+        my $m = Mojolicious::Plugin::Mongodb::Connection->new(mongo_conf => $conf);
+        $m->init();
+        return $m;
     });
+
+    $app->helper('mongodb_connection' => sub { return shift->app->_mongodb->_conn });
 
     $app->helper($conf->{helper} => sub {
         my $self = shift;
-        my $db   = shift || $self->app->defaultdb;
-        return ($db) ? $self->app->mongodb_connection->get_database($db) : undef;
-    }) unless($conf->{nohelper});
-
-    $app->helper('coll' => sub {
-        my $self = shift;
-        my $coll = shift;
-        my $db   = shift || $self->app->defaultdb;
-
-        return undef unless($db && $coll);
-        return $self->app->mongodb_connection->get_database($db)->get_collection($coll);
+        return $self->app->_mongodb->db(@_);
     });
+
+    for my $helpername(qw/coll find_and_modify map_reduce/) {
+        $app->helper($helpername => sub { return shift->app->_mongodb->$helpername(@_) });
+    }
 }
+
+package Mojolicious::Plugin::Mongodb::Connection;
+use Mojo::Base -base;
+use Tie::IxHash;
+
+has 'mongo_conf' => sub { {} };
+has 'current_db';
+has '_conn';
+
+sub init {
+    my $self = shift;
+    
+    $self->_conn(MongoDB::Connection->new($self->mongo_conf));
+}
+
+sub db {
+    my $self = shift;
+    my $db   = shift;
+
+    $self->current_db($db) if($db);
+    return $self->_conn->get_database($self->current_db) if($self->current_db);
+    return undef;
+}
+
+sub coll {
+    my $self = shift;
+    my $coll = shift;
+
+    return $self->db->get_collection($coll);
+}
+
+sub find_and_modify {
+    my $self = shift;
+    my $coll = shift;
+    my %opts = (@_);
+    my $q    = delete($opts{query});
+    my $u    = delete($opts{update});
+
+    my $cmd = Tie::IxHash->new(
+        "findAndModify" => $coll,
+        %opts,
+        );
+
+    return $self->db->run_command($cmd);
+}
+
+sub map_reduce {
+    my $self = shift;
+    my $collection = shift;
+    my %options = (@_);
+    my $as_cursor = delete($options{'as_cursor'});
+
+    my $cmd = Tie::IxHash->new( 
+        "mapreduce" => $collection,
+        %options
+    );
+
+    my $res = $self->db->run_command($cmd);
+    if($res->{ok} == 1) {
+        my $coll = $res->{result};
+        return ($as_cursor) ? $self->coll($coll)->find() : $res;
+    } else {
+        return undef;
+    }
+}
+
 
 1; 
 __END__
@@ -52,60 +112,59 @@ Provides a few helpers to ease the use of MongoDB in your Mojolicious applicatio
         $self->plugin('mongodb', { 
             host => 'localhost',
             port => 27017,
-            database => 'default_database',
             helper => 'db',
             });
     }
 
 =head1 CONFIGURATION OPTIONS
 
-    helper      (optional)  The name to give to the easy-access helper if you want to change it's name from the default
-    no_helper   (optional)  When set to true, no helper will be installed.
-    database    (optional)  Set a default database you want to operate on
+    helper      (optional)  The name to give to the easy-access helper if you want to change it's name from the default 'db' 
 
 All other options passed to the plugin are used to connect to MongoDB.
 
 =head1 HELPERS/ATTRIBUTES
 
-=head2 connection
-
-This attribute has been deprecated in favor of mongodb_connection.
-
 =head2 mongodb_connection
 
-This plugin attribute holds the MongoDB::Connection object, use this if you need to access it for some reason. 
+This plugin helper will return the MongoDB::Connection object, use this if you need to access it for some reason. 
 
-=head2 db([dbname])
+=head2 db([$dbname])
 
-This helper will return the database you specify, if you don't specify one, then the default database is returned. If no default has been set and you have not specified a database name, undef will be returned. If you have renamed the helper, use that name instead of 'db' in the example below :)
+This is the name of the default easy-access helper. The default name for it is 'db', unless you have changed it using the 'helper' configuration option. This helper will return either the database you specify, or the last database you specified. You must use this in order to set the database you wish to operate on for those helpers that specify you should.
 
     sub someaction {
         my $self = shift;
 
-        # select a database yourself
+        # select a database 
         $self->db('my_snazzy_database')->get_collection('foo')->insert({ bar => 'baz' });
 
-        # if you passed 'my_snazzy_database' during plugin load as the default, this is equivalent:
+        # do an insert on the same database
         $self->db->get_collection('foo')->insert({ bar => 'baz' });
-
-        # if you want to be anal retentive about things in case no default exists and no database was passed:
-        $self->db and $self->db->get_collection('foo')->insert({ bar => 'baz' });
     }
 
-=head2 coll(collname, [dbname])
+=head2 coll($collname)
 
-This helper allows easy access to a collection. If you don't pass the dbname argument, it will return the given collection inside the default database. If no default database exists, it will return undef.
+This helper allows easy access to a collection. It requires that you have previously selected a database using the 'db' helper. It will return undef if you have not specified a database first.
 
     sub someaction {
         my $self = shift;
 
-        # get the 'foo' collection in the default database
+        # get the 'foo' collection in the 'bar' database
+        $self->db('bar');
         my $collection = $self->coll('foo');
 
         # get the 'bar' collection in the 'baz' database
-        my $collection = $self->coll('bar', 'baz');
+        $self->db('baz');
+        my $collection = $self->coll('bar');
     }
 
+=head2 find_and_modify($collname, \%options)
+
+This helper executes a 'findAndModify' operation on the given collection. You must have selected a database using the 'db' helper. See  L<http://www.mongodb.org/display/DOCS/findAndModify+Command> for supported options. It will return the raw result from the MongoDB driver. 
+
+=head2 map_reduce($collname, \%options)
+
+This helper executes a 'mapReduce' operation on the given collection. You must have selected a database using the 'db' helper. All options from L<http://www.mongodb.org/display/DOCS/MapReduce> are supported. It will return undef on failure. On success, it will return the raw result from the MongoDB driver, or if you have passed the 'as_cursor' option, it will return a MongoDB::Cursor object for your result collection.
 
 =head1 AUTHOR
 
@@ -152,7 +211,7 @@ Thanks to Henk van Oers for pointing out a few errors in the documentation, and 
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2011 Ben van Staveren.
+Copyright 2011, 2012 Ben van Staveren.
 
 This program is free software; you can redistribute it and/or modify it
 under the terms of either: the GNU General Public License as published
